f9e04954268985415bf96b2c330fadad1c8ce399
miguelcv-eg <miguel.christv123@gmail.com>
2026-01-11 19:10:53 +0700

    Update cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "skill-check-v1";
+const CACHE_NAME = "skill-check-v2";
 
 const FILES_TO_CACHE = [
   "./",
@@ -20,3 +20,4 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
