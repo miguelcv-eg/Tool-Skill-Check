e71fa187a0e74430e551509c5c16b2cc7467de33
miguelcv-eg <miguel.christv123@gmail.com>
2026-01-11 18:27:31 +0700

    Add PWA files

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "skill-check-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./ULTRAKILL Parry Sound Effect.mp3"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
